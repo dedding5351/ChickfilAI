85aec9efdd733c59d7081c9614bc42fe73d1e61c
William Gulian <williamgulian@gmail.com>
2018-11-03 17:05:39 -0400

    Add user and menu data and a bunch of .idea files

--- hosted_relay/main.py
+++ hosted_relay/main.py
@@ -1,28 +1,49 @@
-import socketio
-import eventlet
 import eventlet.wsgi
-from flask import Flask, render_template
+import socketio
+from flask import Flask
+
+from user_data import customerArray
 
 sio = socketio.Server()
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     """Serve the client-side application."""
     return 'Ok'
 
+
 @sio.on('connect', namespace='/relay')
 def connect(sid, environ):
     print('Client {} connected'.format(sid))
 
+
 @sio.on('user_detection', namespace='/relay')
 def message_func(sid, data):
+
+    # data['name'] == whoever's name
+
+    user = None
+    for i_user in customerArray:
+        if i_user.first_name == data['name']:
+            user = i_user
+            break
+
+    if user:
+        data['last_order'] = user.last_order
+        data['order_price'] = user.order_price
+        data['description'] = user.description
+        data['url'] = user.url
+
     print("Relaying from {}: user_detection -> {}".format(sid, str(data)[:100]))
     sio.emit('user_detection', data, skip_sid=sid)
+
 
 @sio.on('disconnect', namespace='/relay')
 def disconnect(sid):
     print('Client {} disconnected'.format(sid))
+
 
 if __name__ == '__main__':
     # wrap Flask application with engineio's middleware
@@ -30,4 +51,3 @@ if __name__ == '__main__':
 
     # deploy as an eventlet WSGI server
     eventlet.wsgi.server(eventlet.listen(('0.0.0.0', 80)), app)
-
